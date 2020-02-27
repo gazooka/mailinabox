56e3e925d233ce4646f08be032c1b9705c409610
Gaz Jones <gazjones00@hotmail.com>
2020-02-27 11:47:20 +0000

    Always copy backups to Azure storage after backup is done

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 ./mail-in-a-box-backup.sh
-./mail-in-a-box-copy-backups-to-azure.sh
 
 ./system-memory-free.sh
 
--- scripts/mail-in-a-box-backup.sh
+++ scripts/mail-in-a-box-backup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 sudo mailinabox/management/backup.py
+./mail-in-a-box-copy-backups-to-azure.sh
