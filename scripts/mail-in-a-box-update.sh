f9ff91d203f2d1f8c7f110a130e5b2e14bb9534c
Gaz Jones <gazjones00@hotmail.com>
2020-02-27 11:45:24 +0000

    Perform backup before upgrading mail-in-a-box

--- scripts/mail-in-a-box-update.sh
+++ scripts/mail-in-a-box-update.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+# if mail-in-a-box has already been installed, perform a backup first in case one hasn't been done
+if sudo test -e "./mailinabox/management/backup.py"; then
+	./mail-in-a-box-backup.sh
+fi
+
+# do the install / upgrade
 curl -s https://mailinabox.email/setup.sh | sudo bash
