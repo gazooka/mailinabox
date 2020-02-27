314f95f19b0c66d1bb71eefea7eb49a8ce9b0787
Gaz Jones <gazjones00@hotmail.com>
2020-02-27 13:25:10 +0000

    Allow parameter to be passed to backup script

--- scripts/mail-in-a-box-backup.sh
+++ scripts/mail-in-a-box-backup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-sudo mailinabox/management/backup.py
+if [ "$1" != "" ]; then
+        sudo mailinabox/management/backup.py $1
+else
+        sudo mailinabox/management/backup.py
+fi
 ./mail-in-a-box-copy-backups-to-azure.sh
